6ed2e85b6eeb8a7be0115efc48883f4dcf477056
Melissa Torres <mmtorre1@gmail.com>
2020-03-08 01:10:56 -0800

    Finally finished the page
    Added the fab and made styling changes. Also added information array.

---
+++ IConRecycle/assets/data/array.js
@@ -0,0 +1,97 @@
+export const information = [
+	{
+		name: "Recyclable Aluminum",
+		description: "This symbol indicates that the item is made from recyclable aluminium.",
+		recycleInfo: "Rinse or wipe off any crumbs or food residue from foil trays. To rinse just dunk the tray in the washing up water - no need to run the tap. Scrunch kitchen foil, tub and pot lids and wrappers together to form a ball - the bigger the ball, the easier it is to recycle.",
+		icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/CEN_recycling_aluminium.svg/170px-CEN_recycling_aluminium.svg.png"
+	},
+	{
+		name: "Compostable",
+		description: "Products certified to be industrially compostable may bear the 'seedling' logo. Never place compostable plastic into the recycling with other plastics; as it is designed to break down it cannot be recycled and contaminates recyclable plastics. Plastics that carry this symbol can be recycled with your garden waste through your local authority.",
+		recycleInfo: "To compost at home, you’ll need browns (dead leaves, branches and/or twigs), greens (grass clippings, vegetable waste, fruit scraps and/or coffee grounds), and water, along with a dry, shady spot for your pile or bin.",
+		icon: ""
+	},
+	{
+		name: "Glass",
+		description: "This symbol asks that you recycle the glass container. Some types of glass do not melt at the same temperature as bottles and jars. If they enter the glass recycling process it can result in new containers being rejected. These items should be recycled separately - check with your local household waste recycling centre.",
+		recycleInfo: "Dispose of glass bottles and jars in a bottle bank, remembering to separate colours, or use your glass household recycling collection if you have one.",
+	},
+	{
+		name: "The Green Dot",
+		description: "The Green Dot does not necessarily mean that the packaging is recyclable, will be recycled or has been recycled. It is a symbol used on packaging in some European countries and signifies that the producer has made a financial contribution towards the recovery and recycling of packaging in Europe.",
+		recycleInfo: "N/A",
+		icon: ""
+	},
+	{
+		name: "Mobius Loop",
+		description: "This indicates that an object is capable of being recycled, not that the object has been recycled or will be accepted in all recycling collection systems. Sometimes this symbol is used with a percentage figure in the middle to explain that the packaging contains x% of recycled material.",
+		recycleInfo: "Check with your local recycling center to see if they will accept that object.",
+		icon: ""
+	},
+	{
+		name: "Plastic #1 - PETE or PET (Polyethylene Terephthalate)",
+		description: "Plastic #1 is usually clear and used to make soda and water bottles and is picked up by most curbside recycling programs. Some consider it safe, but this plastic is known to allow bacteria to accumulate. Avoid recycling.",
+		recycleInfo: "Can be picked up through most curbside recycling programs as long as it's been emptied and rinsed of any food. When it comes to caps, our environmental pros say it’s probably better to dispose of them in the trash, unless your town explicitly says you can throw them in the recycle bin.",
+		icon: ""
+	},
+	{
+		name: "Plastic #2 - HDPE (High Density Polyethylene)",
+		description: "Plastic #2 is typically opaque and picked up by most curbside recycling programs. This plastic is one of the 3 plastics considered to be safe, and has a lower risk of leaching. Safe to recycle.",
+		recycleInfo: "Can often be picked up through most curbside recycling programs, although some allow only containers with necks. Flimsy plastics (like grocery bags and plastic wrap) usually can't be recycled, but some stores will collect and recycle them.",
+		icon: ""
+	},
+	{
+		name: "Plastic #3 – V or PVC (Vinyl)",
+		description: "Plastic #3 is used to make food wrap, plumbing pipes, and detergent bottles, and is seldom accepted by curbside recycling programs. These plastics used to, and still may, contain phthalates, which are linked to numerous health issues ranging from developmental problems to miscarriages. They also contain DEHA, which can be carcinogenic with long-term exposure. DEHA has also been linked to loss of bone mass and liver problems. Don’t cook with or burn this plastic. Avoid recycling.",
+		recycleInfo: "PVC and V can rarely be recycled, but it's accepted by some plastic lumber makers. If you need to dispose of either material, ask your local waste management to see if you should put it in the trash or drop it off at a collection center.",
+		icon: ""
+	},
+	{
+		name: "Plastic #4 – LDPE (Low Density Polyethylene)",
+		description: "Plastic #4 rests among the recycling symbols considered to be safe. LDPE is considered less toxic than other plastics, and relatively safe for use. It is not commonly recycled, however, although this is changing in many communities today as more plastic recycling programs gear up to handle this material. Safe to recycle.",
+		recycleInfo: "LDPE is not often recycled through curbside programs, but some communities might accept it. That means anything made with LDPE (like toothpaste tubes) can be thrown in the trash.",
+		icon: ""
+	},
+	{
+		name: "Plastic #5 – PP (Polypropylene)",
+		description: "Increasingly becoming accepted by curbside recycle programs, plastic #5 is also one of the safer plastics to look for. Safe to recycle.",
+		recycleInfo: "PP can be recycled through some curbside programs, just don't forget to make sure there's no food left inside. It's best to throw loose caps into the garbage since they easily slip through screens during recycling and end up as trash anyways.",
+		icon: ""
+	},
+	{
+		name: "Plastic #6 – PS (Polystyrene)",
+		description: "Polystyrene is Styrofoam, which is notorious for being difficult to recycle, and thus, bad for the environment. This kind of plastic also poses a health risk, leaching potentially toxic chemicals, especially when heated. Most recycling programs won’t accept it. Avoid recycling.",
+		recycleInfo: "Not many curbside recycling programs accept PS in the form of rigid plastics (and many manufacturers have switched to using PET instead). Since foam products tend to break apart into smaller pieces, you should place them in a bag, squeeze out the air, and tie it up before putting it in the trash to prevent pellets from dispersing.",
+		icon: ""
+	},
+	{
+		name: "Plastic #7 – Other, Miscellaneous",
+		description: "It’s a mix bag of plastics that don’t fit into the other categories that includes polycarbonate, which contains the toxic bisphenol-A (BPA). These plastics should be avoided due to possibly containing hormone disruptors like BPA, which has been linked to infertility, hyperactivity, reproductive problems, and other health issues. Avoid recycling.",
+		recycleInfo: "These other plastics are traditionally not recycled, so don't expect your local provider to accept them. The best option is to consult your municipality's website for specific instructions.",
+		icon: ""
+	},
+	{
+		name: "Recyclable Steel",
+		description: "This symbol means that the product is made of steel. All local authorities collect steel cans for recycling. Other steel or metal items can be taken to your household recycling centre.",
+		recycleInfo: "Look for curbside recycling bins, local recycling drop-offs, or scrap buy-back centers.",
+		icon: "N/A"
+	},
+	{
+		name: "Tidyman",
+		description: "This symbol from Keep Britain Tidy asks you not to litter. It doesn't relate to recycling but is a reminder to be a good citizen, disposing of the item in the most appropriate manner.",
+		recycleInfo: "",
+		icon: ""
+	},
+	{
+		name: "Waste Electricals",
+		description: "This symbol explains that you should not place the electrical item in the general waste. Electrical items can be recycled through a number of channels.",
+		recycleInfo: "Simply ask the following questions and if the answer is yes, to any of these, it is recyclable: Does it have a plug? Does it use batteries? Does it need charging? Does it have a picture of a crossed out wheelie bin on it?",
+		icon: ""
+	},
+	{
+		name: "Widely Recycled",
+		description: "This logo denotes that over 75% of England’s councils provide household recycling collection for that packaging type. Part of the label tells you which part of the packaging it refers to, such as BOX or BOTTLE, and the material it consists of, like CARDBOARD or GLASS), helping you easily separate your packaging into recyclables and non-recyclables.",
+		recycleInfo: "Follow the description found above the logo to dispose of the item properly.",
+		icon: ""
+	},
+];
--- IConRecycle/screens/InfographicScreen.js
+++ IConRecycle/screens/InfographicScreen.js
@@ -1,91 +1,56 @@
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { Card } from 'react-native-cards';
 import { ScrollView } from 'react-native-gesture-handler';
-import * as WebBrowser from 'expo-web-browser';
-import Unorderedlist from 'react-native-unordered-list';
 import { FloatingAction } from "react-native-floating-action";
-// import { Icon } from 'react-native-elements'
-import Icon from 'react-native-vector-icons/FontAwesome'
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { Icon } from 'react-native-elements'
+import {information} from '../assets/data/array.js'
 
-import { MonoText } from '../components/StyledText';
+
 
 export default function InfographicScreen() {
   return (
     <View style={styles.container}>
-        <ScrollView style={styles.container}>
-            <View style={styles.container1}></View>
-            <Card style={styles.cardCon}>
-                <Text style={styles.titleCon}>Lorem Ipsum</Text>
-                <Text style={styles.explanationCon}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </Text>
-            </Card>
-            <Card style={styles.cardCon1}>
-                <Text style={styles.titleCon}>Lorem Ipsum Ipsum Lorem:</Text>
-                <Unorderedlist style={styles.listCon}>
-                    <Text>ad minim veniam</Text>
-                </Unorderedlist>
-                <Unorderedlist style={styles.listCon}>
-                    <Text>ad minim veniam</Text>
-                </Unorderedlist>
-                <Unorderedlist style={styles.listCon}>
-                    <Text>ad minim veniam</Text>
-                </Unorderedlist>
-                <Unorderedlist style={styles.listCon}>
-                    <Text>ad minim veniam</Text>
-                </Unorderedlist>
-            </Card>
-        </ScrollView>
-        {/* <FloatingAction style={styles.fab}>
-            <Icon class="fas fa-camera"></Icon>
-        </FloatingAction>
-        <TouchableOpacity style={styles.fab}>
-            <Text style={styles.text}>+</Text>
-            <i class="fas fa-camera"></i>
-            <FontAwesomeIcon icon="coffee" />
-            <Icon name={camera} size={40}/>
-        </TouchableOpacity> */}
+        {showInfo("Recyclable Aluminum")}
     </View>
   );
 }
 
-InfographicScreen.navigationOptions = {
-  header: null,
-};
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-  );
+function showInfo( iconName ) {
+    for (var i=0; i < information.length; i++) {
+        if (iconName == information[i].name) {
+            return (
+                <View style={styles.container}>
+                    <ScrollView style={styles.container}>
+                        <View style={styles.container1}>
+                            <Image
+                                style={styles.iconImage}
+                                source={{uri: information[i].icon}}
+                            />
+                        </View>
+                        <Card style={styles.cardCon}>
+                            <Text style={styles.titleCon}>{information[i].name}</Text>
+                            <Text style={styles.explanationCon}>
+                                {information[i].description}
+                            </Text>
+                        </Card>
+                        <Card style={styles.cardCon1}>
+                            <Text style={styles.titleCon1}>How to recycle:</Text>
+                            <Text style={styles.explanationCon}>
+                                {information[i].recycleInfo}
+                            </Text>
+                        </Card>
+                    </ScrollView>
+                    <FloatingAction
+                        color={'#41A56C'}
+                        floatingIcon={
+                        <Icon
+                            name='camera'
+                            type='font-awesome' color='white'/>}/>
+                </View>            
+            );
+        }
+    }
 }
 
 const styles = StyleSheet.create({
@@ -98,116 +63,38 @@ const styles = StyleSheet.create({
     height: 200,
     backgroundColor: '#81CEA2',
   },
+  iconImage: {
+    margin: 50,
+    width: 100,
+    height: 100
+  },
   cardCon: {
     margin: 20,
     borderRadius: 20
   },
   titleCon: {
     marginLeft: 20,
-    marginTop: 20
+    marginTop: 20,
+    fontWeight: 'bold',
+    fontFamily: 'Avenir',
+    fontSize: 18
   },
   explanationCon: {
-    margin: 20
+    margin: 20,
+    fontFamily: 'Avenir',
+    fontSize: 15
+  },
+
+  titleCon1: {
+    marginLeft: 20,
+    marginTop: 20,
+    fontWeight: 'bold',
+    fontFamily: 'Avenir',
+    fontSize: 18
   },
   cardCon1: {
     marginLeft: 20,
-    marginRight: 20
-  },
-  listCon: {
-    paddingLeft: 20,
-  },
-  fab: {
-    backgroundColor: '#41A56C',
-    height: 50,
-    width: 50,
-    borderRadius: 200,
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-//   homeScreenFilename: {
-//     marginVertical: 7,
-//   },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+    marginRight: 20,
+    borderRadius: 20
+  }
 });
